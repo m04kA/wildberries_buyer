40bb2fcb8b69b4ce235f7ca0b39e1305fd1a9689
m04kA <yarniyjack@gmail.com>
2022-03-16 18:25:02 +0300

    removed useless files

--- test.py
+++
@@ -1,8 +0,0 @@
-import urllib.parse
-
-query = {'spam': 1, 'eggs': True, 'bacon': 'foo'}
-params = '&'.join([
-            str(x) + "=" + urllib.parse.quote_plus(str(y))
-            for x, y in query.items()
-        ])
-print(params)
